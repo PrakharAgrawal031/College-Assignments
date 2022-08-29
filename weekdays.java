578f5b80b45a79b4543246e9ee45c7041ce66862
Prakhar <agrawalprakhar031@gmail.com>
2022-08-30 00:05:30 +0530

    create weekdays.main

--- weekdays.java
+++ weekdays.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 
-public class Demo {
+public class weekdays {
 
     public static void main(String[] args) {
 
-        /* This reads the input provided by user
-         * using keyboard
-         */
+        String[] arr = {"Monday","Tuesday", "Wednesday", "Thursday" ,"friday", "Saturday", "Sunday"};
+        
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter any number: ");
+        System.out.print("Select the day of week: ");
 
-        // This method reads the number provided using keyboard
         int num = scan.nextInt();
 
-        // Closing Scanner after the use
         scan.close();
         
-        // Displaying the number 
-        System.out.println("The number entered by user: "+num);
+        System.out.printf("the day on %dth day of week is %s\n",num, arr[num-1]);
+
     }
 }
