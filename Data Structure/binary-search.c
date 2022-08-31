7f2bc6720428d4db3610e7f810eab6f3910f212c
Prakhar <agrawalprakhar031@gmail.com>
2022-09-01 00:21:44 +0530

    Update binary-search.c

--- Data Structure/binary-search.c
+++ Data Structure/binary-search.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-int binarysearch(int arr[],int size, int item)
+#include <stdio.h>
+int binarysearch(int arr[], int size, int item)
 {
     int low = 0, high = size - 1;
-      while (low<=high)
+    while (low <= high)
     {
-        int mid  = (low+high)/2;
-        if (item==arr[mid])
+        int mid = (low + high) / 2;
+        if (item == arr[mid])
         {
             return mid;
         }
-        else if (item<arr[mid])
+        else if (item < arr[mid])
         {
-            high = mid-1;
+            high = mid - 1;
         }
-        else low = mid+1;
+        else
+            low = mid + 1;
     }
     return -1;
-
 }
 int main(int argc, char const *argv[])
 {
@@ -29,16 +29,17 @@ int main(int argc, char const *argv[])
     {
         scanf("%d", &arr[i]);
     }
-    
+
     int item;
     printf("\nEnter the item you wish to search: ");
     scanf("%d", &item);
     int position = -1;
-    position = binarysearch(arr,size, item) + 1;
-    if (position!=0)
+    position = binarysearch(arr, size, item) + 1;
+    if (position != 0)
     {
         printf("\nItem is at %d position", position);
     }
-    else printf("\nItem not found");
+    else
+        printf("\nItem not found");
     return 0;
 }
