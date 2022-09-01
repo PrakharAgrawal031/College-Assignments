e6cd346768133797209d8a369bf4d36e87f8eecd
Prakhar <agrawalprakhar031@gmail.com>
2022-09-01 23:08:23 +0530

    Update sum-avg.c

--- Object-Oriented-Programming/sum-avg.c
+++ Object-Oriented-Programming/sum-avg.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    int arr[5],sum=0,avg;
-    float perc;
+    int arr[5],sum=0;
+    float perc, avg;
     printf("Enter the marks: ");
     for (int i = 0; i < 5; i++)
     {
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[])
     }
     avg = sum/5;
     perc = (sum*100)/500;
-    printf("Average = %d, Percent = %.2f, Grade = ", avg, perc);
+    printf("Average = %.2f, Percent = %.2f, Grade = ", avg, perc);
     if (perc>90)
     {
         printf("A+");
