1ee6f3ce772984bee2639b5694b80b98a6829c0f
Prakhar <agrawalprakhar031@gmail.com>
2022-09-22 21:27:50 -0700

    Create dynamic-obj.cpp

---
+++ Object-Oriented-Programming/dynamic-obj.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Test
+{
+	int a, b;
+	public:
+	
+		Test()
+		{
+			cout << "Constructor is called" <<
+					endl;
+			a = 1;
+			b = 2;
+		};
+	
+		~Test()
+		{
+			cout << "Destructor is called" <<
+					endl;
+		}
+	
+		void show()
+		{
+			cout << "a = " <<
+					a << endl;
+			cout << "b = " <<
+					b << endl;
+		}
+};
+
+int main()
+{
+	Test *ptr;	
+	ptr = new Test;
+
+	ptr->show();	
+
+	delete ptr;	
+	return 0;
+}
