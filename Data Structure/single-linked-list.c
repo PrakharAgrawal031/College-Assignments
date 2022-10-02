2dba3a2e03e77ededb1c1583c5d90810d0a60b87
Prakhar <agrawalprakhar031@gmail.com>
2022-10-02 22:37:17 +0530

    Update single-linked-list.c

--- Data Structure/single-linked-list.c
+++ Data Structure/single-linked-list.c
@@ -8,105 +8,45 @@ struct node
 };
 typedef struct node node_type;
 
-node_type *create_node(int value)
-{
-    node_type *result = malloc(sizeof(node_type));
-    result->value = value;
-    result->next = NULL;
-}
-
-node_type *find_node(node_type *head, int value)
-{
-    node_type *temp = head;
-    while (temp != NULL)
-    {
-        if (temp->value == value)
-            return temp;
-        temp = temp->next;
-    }
-}
-node_type *find_end(node_type *head, int size)
-{
-    node_type *temp = head;
-    for (int i = 0; i < size; i++)
-    {
-        if (i == size - 1)
-            return temp;
-        temp = temp->next;
-    }
-}
-
-node_type *insert_at_head(node_type **head, node_type *newnode)
-{
-    newnode->next = *head;
-    *head = newnode;
-    return newnode;
-}
-void insert_random(node_type *node_to_insert_after, node_type *newnode)
-{
-    newnode->next = node_to_insert_after->next;
-    node_to_insert_after->next = newnode;
-}
-node_type *insert_at_end(node_type *temp, node_type *newnode)
-{
-    temp->next = newnode;
-}
-
-node_type *delete_val(node_type **head, int position)
-{
-    node_type *current = *head;
-    node_type *previous = *head;
-    if (position == 1)
-    {
-        *head = current->next;
-        free(current);
-        current = NULL;
-    }
-    else
-        while (position != 1)
-        {
-            previous = current;
-            current = current->next;
-            position--;
-        }
-    previous->next = current->next;
-    free(current);
-    current = NULL;
-}
-
-void printlist(node_type *head)
-{
-    node_type *temp = head;
-    while (temp != NULL)
-    {
-        printf("%d  ", temp->value);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
 int main()
 {
-    node_type *head, *temp1, *temp2;
-    int num, position, nodevalue, searchval, choice1 , choice2;
+    node_type *head, *temp, *result, *p_temp, *current, *previous;
+    int num, position, nodevalue, searchval, choice1, choice2;
 
     // creating initial linked list
     printf("Enter the number of nodes: ");
     scanf("%d", &num);
     printf("Enter the value of node 1:");
     scanf("%d", &nodevalue);
-    temp1 = create_node(nodevalue);
-    insert_at_head(&head, temp1);
+    result = malloc(sizeof(node_type));
+    result->value = nodevalue;
+    result->next = head;
+    head = result;
     for (int i = 1; i < num; i++)
     {
         printf("Enter the value of node %d:", i + 1);
         scanf("%d", &nodevalue);
-        temp1 = create_node(nodevalue);
-        temp2 = find_end(head, i); // finding the end of list
-        insert_at_end(temp2, temp1);
+        result = malloc(sizeof(node_type));
+        result->value = nodevalue;
+        result->next = NULL;
+        temp = head;
+        for (int j = 0; j < i; j++)
+        {
+            if (j == i - 1)
+                break;
+            temp = temp->next;
+        }
+
+        temp->next = result;
     }
 
-    printlist(head);
+    p_temp = head;
+    while (p_temp != NULL)
+    {
+        printf("%d  ", p_temp->value);
+        p_temp = p_temp->next;
+    }
+    printf("\n");
     printf("Select the operation to perform: \n");
     printf(" - To insert a new element in list press 1\n");
     printf(" - To delete an existing element in list press 2\n");
@@ -127,28 +67,68 @@ int main()
         {
             printf("Enter the value of node ");
             scanf("%d", &nodevalue);
-            insert_at_head(&head, create_node(nodevalue));
-            printlist(head);
+            result = malloc(sizeof(node_type));
+            result->value = nodevalue;
+            result->next = head;
+            head = result;
+            p_temp = head;
+            while (p_temp != NULL)
+            {
+                printf("%d  ", p_temp->value);
+                p_temp = p_temp->next;
+            }
+            printf("\n");
             return 0;
         }
         else if (choice2 == 2)
         {
             printf("\nEnter the value of node: ");
             scanf("%d", &nodevalue);
-            temp2 = find_end(head,num);
-            insert_at_end(temp2, create_node(nodevalue));
-            printlist(head);
+            temp = head;
+            for (int j = 0; j < num; j++)
+            {
+                if (j == num - 1)
+                    break;
+                temp = temp->next;
+            }
+            result = malloc(sizeof(node_type));
+            result->value = nodevalue;
+            result->next = NULL;
+            temp->next = result;
+            p_temp = head;
+            while (p_temp != NULL)
+            {
+                printf("%d  ", p_temp->value);
+                p_temp = p_temp->next;
+            }
+            printf("\n");
             return 0;
         }
         else if (choice2 == 3)
         {
             printf("Enter the value of element after which you want to insert a new element: ");
             scanf("%d", &searchval);
-            temp1 = find_node(head, searchval);
+            temp = head;
+            while (temp != NULL)
+            {
+                if (temp->value == searchval)
+                    break;
+                temp = temp->next;
+            }
+
             printf("\nEnter the value of node: ");
             scanf("%d", &nodevalue);
-            insert_random(temp1, create_node(nodevalue));
-            printlist(head);
+            result = malloc(sizeof(node_type));
+            result->value = nodevalue;
+            result->next = temp->next;
+            temp->next = result;
+            p_temp = head;
+            while (p_temp != NULL)
+            {
+                printf("%d  ", p_temp->value);
+                p_temp = p_temp->next;
+            }
+            printf("\n");
             return 0;
         }
         else
@@ -158,16 +138,47 @@ int main()
     case 2:
         printf("Enter the position of element you wish to delete:\n ");
         scanf("%d", &position);
-        delete_val(&head, position);
-        printlist(head);
+        // delete_val(&head, position);
+        current = head;
+        previous = head;
+        if (position == 1)
+        {
+            head = current->next;
+            free(current);
+            current = NULL;
+        }
+        else
+            while (position != 1)
+            {
+                previous = current;
+                current = current->next;
+                position--;
+            }
+        previous->next = current->next;
+        free(current);
+        current = NULL;
+        p_temp = head;
+        while (p_temp != NULL)
+        {
+            printf("%d  ", p_temp->value);
+            p_temp = p_temp->next;
+        }
+        printf("\n");
         break;
 
     case 3:
         printf("Enter the value to search: ");
         scanf("%d", &searchval);
-        temp1 = find_node(head, searchval);
-        if (temp1 != NULL)
-            printf("\nFound %d at %p\n", temp1->value, temp1);
+        temp = head;
+        while (temp != NULL)
+        {
+            if (temp->value == searchval)
+                break;
+            temp = temp->next;
+        }
+
+        if (temp != NULL)
+            printf("\nFound %d at %p\n", temp->value, temp);
         else
             printf("Value not found\n");
         break;
