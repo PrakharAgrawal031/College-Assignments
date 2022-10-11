5a1f4a727da8ff8fa806ded671e1b1a8587f80f2
Prakhar <agrawalprakhar031@gmail.com>
2022-10-10 21:02:31 -0700

    Create fibonacci_triangle.java

---
+++ JAVA/fibonacci_triangle.java
@@ -0,0 +1,20 @@
+public class fibonaccipattern
+{
+	public static void main(String[] args) {
+	 int count1 =1;
+	 int count2 = 1, temp;
+	 System.out.println("0 ");
+	 System.out.printf("1 1 \n");
+		for (int i = 3;i<5 ;i++)
+		{
+		 for(int j = 0; j<i; j++)
+		 {
+		  temp = count2;
+		  count2=count2+count1;
+		  count1 = temp;
+		  System.out.printf("%d ", count2);
+		 }
+		 System.out.printf("\n");
+		}
+	}
+}
