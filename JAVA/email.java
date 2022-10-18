2c1e2465b04c0f485d7207b5f6464028c8a2079b
Prakhar <agrawalprakhar031@gmail.com>
2022-10-18 10:17:16 +0530

    Create email.java

---
+++ JAVA/email.java
@@ -0,0 +1,19 @@
+/*Input an email form the user. You have to create a user name from email id
+ * by deleting the part after "@"*/
+
+import java.util.Scanner;
+public class email {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your email: ");
+        String E1 = in.nextLine();
+        in.close();
+        int x = E1.indexOf('@');
+        String username = new String();
+        for(int  i = 0; i<x; i++)
+        {
+            username += E1.charAt(i);
+        }
+        System.out.println("Username = " + username);
+    }
+}
