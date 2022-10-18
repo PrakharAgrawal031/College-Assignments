d44244e8fc10d5c734d0f719871c591fa8e49bb7
Prakhar <agrawalprakhar031@gmail.com>
2022-10-18 10:17:24 +0530

    Create package_use.java

---
+++ JAVA/user_package/package_use.java
@@ -0,0 +1,8 @@
+package user_package;
+public class package_use {
+    public static void main(String[] args) {
+        user_package obj = new user_package();
+        package_use.print();
+    }
+}
+
