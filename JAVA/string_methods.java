0d64c1d208bcba21ad3cae74bc5c78f1df5da0f4
Prakhar <agrawalprakhar031@gmail.com>
2022-10-18 10:17:21 +0530

    Create string_methods.java

---
+++ JAVA/string_methods.java
@@ -0,0 +1,11 @@
+public class string_methods {
+
+    public static void main(String[] args) {
+        String S1 = "Hello";
+        String S2  = " World!";
+        System.out.println(S1+S2);
+        System.out.println(S1.compareTo(S2));
+        
+    }
+    
+}
