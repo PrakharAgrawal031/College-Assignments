077c29be1dde59141d3ebaf7fba763835d9be0fa
Prakhar <agrawalprakhar031@gmail.com>
2022-11-05 09:10:16 +0530

    Create multilevel-inheritance.cpp

---
+++ Object-Oriented-Programming/multilevel-inheritance.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Vehicle
+{
+    public:
+    Vehicle(){cout<<"Obj is of type Vehicle."<<endl;}
+};
+class Car: public Vehicle
+{
+    public:
+    Car(){cout<<"This vehicle is known as car."<<endl;}
+};
+class Company: public Car
+{
+    public:
+    Company(){cout<<"Car is of company BMW";}
+};
+int main()
+{
+    Company Obj1;
+}
