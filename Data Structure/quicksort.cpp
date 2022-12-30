f917c75aec2d3534d699805429109272fa3ebd87
Prakhar <agrawalprakhar031@gmail.com>
2022-12-30 09:59:27 +0530

    Update quicksort.cpp

--- Data Structure/quicksort.cpp
+++ Data Structure/quicksort.cpp
@@ -13,7 +13,7 @@ int partition(int arr[], int low, int high)
 	int i = (low- 1); 
 	for (int j = low; j <= high - 1; j++) {
 		
-		if (arr[j] > pivot) {
+		if (arr[j] < pivot) {
 			i++; 
 			swap(&arr[i], &arr[j]);
 		}
