06c8a0e098066ca0e414d5b4a6a1328c26e70e52
Prakhar <agrawalprakhar031@gmail.com>
2023-02-06 09:38:47 +0530

    Create IndexArrayList.java

---
+++ Advance Java Programming/IndexArrayList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class IndexArrayList{
+    public static void main(String args[]){
+        ArrayList a = new ArrayList();
+        a.add(1);
+        a.add(2);
+        a.add("Hello World");
+        System.out.println(a);
+        System.out.println("Element at index 2 of ArrayList is " + a.get(2));
+        
+        
+    }
+}
