f78430b6569aac0951908f94633a24ce97f6fe97
Prakhar <agrawalprakhar031@gmail.com>
2023-02-06 09:32:32 +0530

    Create SizeArrayList.java

---
+++ Advance Java Programming/SizeArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SizeArrayList{
+    public static void main(String args[]){
+        ArrayList a = new ArrayList();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        System.out.println(a);
+        System.out.println("Size of ArrayList = " + a.size());
+        
+    }
+}
