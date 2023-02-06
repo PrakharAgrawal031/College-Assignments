0534637f4d6975c14870327884578c421286824a
Prakhar <agrawalprakhar031@gmail.com>
2023-02-06 09:52:25 +0530

    Create LinkedListConstructor.java

---
+++ Advance Java Programming/LinkedListConstructor.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class LinkedListConstructor{
+    public static void main(String args[])
+    {
+        LinkedList l = new LinkedList();
+        l.addLast("SUP");
+        l.addLast("Hello");
+        System.out.println(l);
+    }
+}
