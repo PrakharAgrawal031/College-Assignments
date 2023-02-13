898498c0668078cd14a6776619972df504c474b5
Prakhar <agrawalprakhar031@gmail.com>
2023-02-13 09:59:23 +0530

    Create Iteratorprac.java

---
+++ Advance Java Programming/Iteratorprac.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Iteratorprac {
+    public static void main(String[] args){
+    ArrayList<String> v = new ArrayList<String>();
+    
+    v.add("Heyy");
+    v.add("World");
+    
+    Iterator<String> en = v.iterator(); 
+     System.out.println("sadfgkh");
+    while(en.hasNext())
+    {
+        System.out.println(en.next());
+    }
+    }
+}
